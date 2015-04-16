93dd1fe33204421f1bfe78d471fd6e9e027d2501
Taha Husain <taha.husain@weboniselab.com>
2015-04-16 15:04:48 +0530

    done till halve. 13 checks failed.  (But 91 succeeded.)

--- src/recursion.clj
+++ src/recursion.clj
@@ -75,8 +75,9 @@
 (defn seq= [seq-1 seq-2]
   (cond
     (and (empty? seq-1) (empty? seq-2)) true
-    (= (first seq-1) (first seq-2)) (seq= (rest seq-1) (rest seq-2))
-    :else false))
+    (not (= (first seq-1) (first seq-2))) false
+    (or (empty? seq-1) (empty? seq-2)) false
+    :else (seq= (rest seq-1) (rest seq-2))))
 
 (defn my-map [pred seq-1 seq-2]
   (if (or (empty? seq-1) (empty? seq-2))
@@ -119,25 +120,42 @@
     (cons (sort (seq a-seq)) (inits (reverse (rest a-seq)))))))
 
 (defn rotations [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    '(())
+    (cons (seq a-seq) (rest (reverse (rest (map concat (tails a-seq) (reverse (inits a-seq)))))))))
 
-(defn my-frequencies-helper [freqs a-seq]
-  [:-])
+(defn frequencies-helper [freqs a-seq]
+  (if (empty? a-seq)
+    freqs
+    (let [freqs-2 (assoc freqs
+                        (first a-seq)
+                        (inc (get freqs (first a-seq) 0)))]
+      (frequencies-helper freqs-2 (rest a-seq)))))
 
 (defn my-frequencies [a-seq]
-  [:-])
+  (frequencies-helper {} a-seq))
 
 (defn un-frequencies [a-map]
-  [:-])
+  (if (empty? a-map)
+    ()
+    (concat (repeat (last (first a-map)) (first (first a-map))) (un-frequencies (rest a-map)))))
 
 (defn my-take [n coll]
-  [:-])
+  (if (or (zero? n) (empty? coll))
+    ()
+    (cons (first coll) (my-take (dec n) (rest coll)))))
 
 (defn my-drop [n coll]
-  [:-])
+  (if (zero? n)
+    coll
+    (my-drop (dec n) (rest coll))))
 
 (defn halve [a-seq]
-  [:-])
+  (let [n (count a-seq)
+        half-n (int (/ n 2))]
+    (if (zero? n)
+      ()
+      (cons (my-take half-n a-seq) (cons (my-drop half-n a-seq) ())))))
 
 (defn seq-merge [a-seq b-seq]
   [:-])
